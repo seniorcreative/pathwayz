9a8e89c974761fb4f144371529b5ab59b15ae872
Steven Smith <steve@seniorcreative.com>
2016-02-02 18:24:34 +1100

    First commit

--- Pathwayz/AppDelegate.swift
+++ Pathwayz/AppDelegate.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
 
+    
+    let locationManager = CLLocationManager() // Add this statement
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+//        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Sound, .Alert, .Badge], categories: nil))
+//        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        
         return true
     }
 
@@ -40,6 +50,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
+//    func handleRegionEvent(region: CLRegion!) {
+//        // Show an alert if application is active
+//        if UIApplication.sharedApplication().applicationState == .Active {
+//            if let message = notefromRegionIdentifier(region.identifier) {
+//                if let viewController = window?.rootViewController {
+//                    showSimpleAlertWithTitle(nil, message: message, viewController: viewController)
+//                }
+//            }
+//        } else {
+//            // Otherwise present a local notification
+//            var notification = UILocalNotification()
+//            notification.alertBody = notefromRegionIdentifier(region.identifier)
+//            notification.soundName = "Default";
+//            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+//        }
+//    }
+//    
+//    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+//        if region is CLCircularRegion {
+//            handleRegionEvent(region)
+//        }
+//    }
+//    
+//    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+//        if region is CLCircularRegion {
+//            handleRegionEvent(region)
+//        }
+//    }
 
 
 }
--- Pathwayz/ViewController.swift
+++ Pathwayz/ViewController.swift
@@ -7,17 +7,121 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    @IBOutlet weak var latField: UITextField!
+    @IBOutlet weak var longField: UITextField!
+    
+    // Constants
+    
+    
+    let regionRadius: CLLocationDistance = 20
+    
+    var myLocations: [CLLocationCoordinate2D] = []
+    
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+                
+                self.locationManager.delegate = self
+                self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                self.locationManager.startUpdatingLocation()
+//                self.locationManager.startUpdatingHeading()
+//                self.locationManager.startMonitoringSignificantLocationChanges()
+                self.locationManager.distanceFilter = 1
+            
+                //self.mapView setup to show user location
+                self.mapView.delegate = self
+                self.mapView.showsUserLocation = true
+                self.mapView.mapType = MKMapType(rawValue: 0)!
+                self.mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+
+        }
+        
+        
+
+
+        
+    }
+    
+
+    /// Custom methods
+    
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        
+
+        self.mapView.setRegion(coordinateRegion, animated: true)
+        
+//        self.locationManager.stopUpdatingLocation()
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+
+
+            let locValue:CLLocationCoordinate2D = locations[0].coordinate
+            self.latField.text = String(locValue.latitude)
+            self.longField.text = String(locValue.longitude)
+
+
+         myLocations.append(locValue)
+ 
+         if(self.mapView.overlays.count > 0)
+         {
+            self.mapView.removeOverlay(self.mapView.overlays[0])
+         }
+        
+         let polyline = MKPolyline(coordinates: &myLocations, count: self.myLocations.count)
+        
+            
+         self.mapView.addOverlay(polyline)
+        
+        if(self.myLocations.count < 1)
+        {
+            centerMapOnLocation(manager.location!)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        print("Error in location manager \(error)")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if (overlay is MKPolyline) {
+            let pr = MKPolylineRenderer(overlay: overlay)
+            pr.strokeColor = UIColor(colorLiteralRed: 0/255, green: 204/255, blue: 204/255, alpha: 0.7)
+            pr.lineWidth = 10
+            pr.alpha = 0.7
+            return pr
+        }
+        else
+        {
+            return MKOverlayRenderer()
+        }
+        
     }
 
 
