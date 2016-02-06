4117c568a315b85d6797873dedd09f20ccd62510
Steven Smith <steve@seniorcreative.com>
2016-02-06 12:09:32 +1100

    Tab bar controller added with custom image (not hooked up yet)

---
+++ Pathwayz/MyTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MyTabBarController.swift
+//  Pathwayz
+//
+//  Created by Steven Smith on 6/02/2016.
+//  Copyright © 2016 Steven Smith. All rights reserved.
+//
+
+import Cocoa
+
+class MyTabBarController: UITabBarController {
+
+}
